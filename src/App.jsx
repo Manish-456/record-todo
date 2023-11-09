6c295c29d05abdee1be5d01f21b94d1745e6e31a
Manish <tamangmanish3456@gmail.com>
2023-11-09 20:25:21 +0545

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -52,8 +52,9 @@ function App() {
       <div className="bg-[#172842] min-h-screen">
         <div className="max-w-4xl p-4 text-gray-50 mx-auto w-full flex flex-col">
           <h2 className="text-sky-400 text-center font-semibold text-xl font-sans">
-            Manage your Todos
+           Manage your Todos
           </h2>
+          <p className="mt-2 text-sm  text-center text-slate-400 font-semibold">A task management app for organizing to-do lists and tracking daily responsibilites.</p>
           <div className="max-w-xl mx-auto h-[0.1px] mt-2 w-full bg-slate-50/40" />
 
           <div className="my-8">
